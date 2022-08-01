717ef5ab5ea53ca1bd7ce0068ffd5c6a54a66b7c
Meghojyoti Pal <meghojp@gmail.com>
2022-08-01 23:53:35 +0530

    Minor change to adjust

--- src/components/layout/Description.jsx
+++ src/components/layout/Description.jsx
@@ -1,6 +1,6 @@
 function Description() {
   return (
-    <div className={`grid grid-cols-1 xl:grid-cols-2 gap-8 mb-4`}>
+    <div className={`grid grid-cols-1 gap-8 mb-4`}>
       <h1 className=' text-6xl mb-1'>Github Lookup</h1>
       <p className='text-3xl mb-2'>
         {' '}
